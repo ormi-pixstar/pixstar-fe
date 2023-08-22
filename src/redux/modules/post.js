105147bc8758fc5373bb14f4ebf99c689a07300e
LJS0000 <jisu2281l@naver.com>
2023-08-22 16:54:08 +0900

    ✏️ FIX: redux thuck 함수명 변경

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -6,17 +6,20 @@ import setToken from '../../axios/setToken';
 const initialState = [];
 
 // 포스트 조회 및 검색
-export const __postList = createAsyncThunk('postList', async (payload, api) => {
-  try {
-    const res = await axios.get(`list/`, {
-      params: {
-        ...payload,
-      },
-    });
-  } catch (err) {
-    return api.rejectWithValue(err);
+export const __getPostList = createAsyncThunk(
+  'getPostList',
+  async (payload, api) => {
+    try {
+      await axios.get(`list/`, {
+        params: {
+          ...payload,
+        },
+      });
+    } catch (err) {
+      return api.rejectWithValue(err);
+    }
   }
-});
+);
 
 export const postSlice = createSlice({
   name: 'post',
@@ -24,10 +27,10 @@ export const postSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(__postList.fulfilled, (state, action) => {
+      .addCase(__getPostList.fulfilled, (state, action) => {
         console.log(action.payload);
       })
-      .addCase(__postList.rejected, (state, action) => {
+      .addCase(__getPostList.rejected, (state, action) => {
         console.log(action.payload);
       });
   },
