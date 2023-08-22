cbf663faaa6d82351a518b3e9b2b43f7fefe4a64
LJS0000 <jisu2281l@naver.com>
2023-08-22 16:37:45 +0900

    ✨ FEATURE: 메인 페이지 헤더 구현

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+      <a href='/login'>로그인</a>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,12 @@
+import Header from '../components/common/Header.jsx';
+
 const MainPage = () => {
-  return <div className='text-blue-500 text-xl font-bold'>메인 페이지</div>;
+  return (
+    <>
+      <Header />
+      <div className='text-blue-500 text-xl font-bold'>메인 페이지</div>;
+    </>
+  );
 };
 
 export default MainPage;
