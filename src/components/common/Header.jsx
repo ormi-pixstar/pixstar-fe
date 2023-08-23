51b5260c56cc1fef6493fab2972f9ade240b79de
LJS0000 <jisu2281l@naver.com>
2023-08-24 00:23:21 +0900

    ⚡️ IMPRV: 헤더에 뒤로가기 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
+  const navigate = useNavigate();
+  const path = window.location.pathname;
+
   return (
-    <header>
+    <header className='flex gap-2'>
+      {path === '/' ? null : (
+        <button onClick={() => navigate(-1)} className='block md:hidden'>
+          {'<'}
+        </button>
+      )}
       <h1>Pixstar</h1>
     </header>
   );
