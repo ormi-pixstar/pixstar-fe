40a0eefb2ca36066302fe85e73529caaf9faed71
LJS0000 <jisu2281l@naver.com>
2023-08-24 00:36:06 +0900

    ✨ FEATURE: 포스트 디테일 페이지 구현
    백 서버 개발 중으로 추후에 수정 필요

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,4 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { __getPostDetail } from '../redux/modules/post';
+import Header from '../components/common/Header.jsx';
+import DetailCard from '../components/detailpage/DetailCard.jsx';
+
 const DetailPage = () => {
-  return <div>상세페이지 입니다.</div>;
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+  const [post, setPost] = useState({});
+
+  const getPostDetail = async () => {
+    await dispatch(__getPostDetail(id))
+      .then((res) => {
+        setPost(res.payload.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getPostDetail();
+  }, []);
+  return (
+    <>
+      <Header />
+      <DetailCard post={post} />
+    </>
+  );
 };
 export default DetailPage;
--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -20,16 +20,37 @@ export const __getPostList = createAsyncThunk(
   }
 );
 
+// 포스트 상세 정보
+export const __getPostDetail = createAsyncThunk(
+  'getPostDetail',
+  async (payload, api) => {
+    try {
+      const res = await axios.get(`detail/${payload}/`);
+      return api.fulfillWithValue(res.data);
+    } catch (err) {
+      return api.rejectWithValue(err.stack);
+    }
+  }
+);
+
 export const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // 포스트 목록 조회
       .addCase(__getPostList.fulfilled, (state, action) => {
         // console.log(action.payload);
       })
       .addCase(__getPostList.rejected, (state, action) => {
+        // console.log(action.payload);
+      })
+      // 포스트 상세 조회
+      .addCase(__getPostDetail.fulfilled, (state, action) => {
+        // console.log(action.payload);
+      })
+      .addCase(__getPostDetail.rejected, (state, action) => {
         // console.log(action.payload);
       });
   },
