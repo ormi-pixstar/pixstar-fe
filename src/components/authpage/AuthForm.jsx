6d7e12ae21ae54d17253207a62ed093471824139
LJS0000 <jisu2281l@naver.com>
2023-08-24 15:05:48 +0900

    ✨ FEATURE: 로그인, 회원가입 API 연결
    백 서버 구현 중으로 테스트 필요

--- src/components/authpage/AuthForm.jsx
+++ src/components/authpage/AuthForm.jsx
@@ -1,14 +1,37 @@
 import SubmitBtn from '../common/SubmitBtn.jsx';
 
-const AuthForm = ({ formType }) => {
+const AuthForm = ({ formType, setAuthData }) => {
+  const submitAuthForm = (e) => {
+    e.preventDefault();
+
+    const email = e.target.querySelector('input[name="email"]').value;
+    const password = e.target.querySelector('input[name="password"]').value;
+    const formValue = { email, password };
+
+    if (formType === 'signup') {
+      formValue.confirmPassword = e.target.querySelector(
+        'input[name="confirmPassword"]'
+      ).value;
+      formValue.nickname = e.target.querySelector(
+        'input[name="nickname"]'
+      ).value;
+    }
+
+    setAuthData(formValue);
+  };
+
   return (
-    <form>
-      <input placeholder='이메일' />
-      <input placeholder='비밀번호' />
+    <form onSubmit={(e) => submitAuthForm(e)}>
+      <input name='email' placeholder='이메일' type='email' />
+      <input name='password' placeholder='비밀번호' type='password' />
       {formType === 'login' ? null : (
         <>
-          <input placeholder='비밀번호 재확인' />
-          <input placeholder='닉네임' />
+          <input
+            name='confirmPassword'
+            placeholder='비밀번호 재확인'
+            type='password'
+          />
+          <input name='nickname' placeholder='닉네임' type='text' />
         </>
       )}
       <SubmitBtn
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { __signup, __login } from '../redux/modules/user.js';
 import AuthForm from '../components/authpage/AuthForm.jsx';
 
 const AuthPage = ({ formType }) => {
+  const dispatch = useDispatch();
+
+  const [authData, setAuthData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const submitAuthData = async () => {
+    await dispatch(
+      formType === 'signup' ? __signup(authData) : __login(authData)
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    submitAuthData();
+  }, []);
+
   return (
     <>
-      <AuthForm formType={formType} />
+      <AuthForm formType={formType} setAuthData={setAuthData} />
     </>
   );
 };
