5d0f657784a6beb2ad0f38ed04ddf5c5bc925aec
LJS0000 <jisu2281l@naver.com>
2023-09-03 07:09:54 +0900

    ✏️ FIX: 네비게이션바 검색 항목 추가

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -7,19 +7,25 @@ const Navbar = () => {
         <li>
           <Link to='/profile'>
             <i className='mr-2 fas fa-user fa-2x'></i>
-            <span className='hidden md:inline-block'>Profile</span>
+            <span className='hidden md:inline-block'>마이페이지</span>
           </Link>
         </li>
         <li>
           <Link to='/'>
             <i className='mr-2 fas fa-home fa-2x'></i>
-            <span className='hidden md:inline-block'>Home</span>
+            <span className='hidden md:inline-block'>홈</span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/'>
+            <i className='mr-2 fas fa-search fa-2x'></i>
+            <span className='hidden md:inline-block'>검색</span>
           </Link>
         </li>
         <li>
           <Link to='/login'>
             <i className='mr-2 fas fa-sign-in-alt fa-2x'></i>
-            <span className='hidden md:inline-block'>Login</span>
+            <span className='hidden md:inline-block'>로그인</span>
           </Link>
         </li>
       </ul>
