2b5a1eab15bb679c798ed598e44cdc97d3cf7c65
LJS0000 <jisu2281l@naver.com>
2023-09-03 05:29:16 +0900

    ✨ FEATURE: 댓글 리덕스 모듈 추가

---
+++ src/redux/modules/comment.js
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import axios from '../../axios/axios.js';
+
+const initialState = {
+  comments: [],
+};
+
+// 댓글 조회 및 검색
+export const __getComments = createAsyncThunk(
+  'getComments',
+  async (payload, api) => {
+    try {
+      const res = await axios.get(`post/detail/${payload}/comment/`);
+      console.log(res);
+      //   return api.fulfillWithValue(res.data);
+    } catch (err) {
+      return api.rejectWithValue(err.stack);
+    }
+  }
+);
+
+export const commentSlice = createSlice({
+  name: 'comment',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // 댓글 목록 조회
+      .addCase(__getComments.fulfilled, (state, action) => {
+        // state.postList = action.payload.results;
+      })
+      .addCase(__getComments.rejected, (state, action) => {
+        console.log(action.payload); // 예외처리 추가 필요
+      });
+  },
+});
+
+export default commentSlice.reducer;
