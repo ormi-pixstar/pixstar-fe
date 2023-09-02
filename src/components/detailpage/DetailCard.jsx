c0142fda51f94272a0f8b09c49383530f25dc232
LJS0000 <jisu2281l@naver.com>
2023-09-03 03:12:15 +0900

    ✨ FEATURE: 글 목록 데이터바인딩 값 수정 및 CSS 일부 작업

--- src/components/detailpage/DetailCard.jsx
+++ src/components/detailpage/DetailCard.jsx
@@ -1,9 +1,9 @@
 const DetailCard = ({ post }) => {
   return (
     <main>
-      <p>작성자: {post.writer}</p>
+      {/* <p>작성자: {post.writer}</p>
       <img src={post.images} />
-      <p>내용: {post.content}</p>
+      <p>내용: {post.content}</p> */}
       <div>
         <i class='far fa-heart'></i>
         <i class='fas fa-heart'></i>
--- src/components/mainpage/ListCard.jsx
+++ src/components/mainpage/ListCard.jsx
@@ -1,22 +1,40 @@
 import { useNavigate } from 'react-router-dom';
+import profilePlaceholder from '../../assets/images/profilePlaceholder.svg';
 
 const ListCard = ({ posts }) => {
   const navigate = useNavigate();
 
   return (
     <main>
-      <article>
+      <article className='flex flex-col gap-4'>
         {posts?.map((post, id) => {
           return (
             <div
               key={id}
-              className='border'
+              id='postCard'
+              className='rounded-lg p-4 flex flex-col gap-2'
               onClick={() => navigate(`/detail/${post.id}`)}
             >
-              <div> 이미지: </div>
-              <div> 내용: {post.content}</div>
-              <div> 작성자: {post.writer}</div>
-              <div> 좋아요 한 사람: {post.like}</div>
+              <div className='flex gap-2 items-center'>
+                <img
+                  src={post.writer.profile_img || profilePlaceholder}
+                  alt='프로필사진'
+                  className='w-12 h-12 rounded-full'
+                />
+                <p className='text-lg'>{post.writer.username}</p>
+              </div>
+              <div>
+                <div className='text-lg mb-4'> {post.content}</div>
+                <img
+                  src={post.image_urls[0].image_url}
+                  alt='게시글 사진'
+                  className='max-w-[50vh] h-[40vh] object-cover rounded-lg shadow-md'
+                />
+              </div>
+              <div className='flex gap-2 items-center'>
+                <i className='far fa-heart text-red-500'></i>
+                {post.like.length}
+              </div>
             </div>
           );
         })}
