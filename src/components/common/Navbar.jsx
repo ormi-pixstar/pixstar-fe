8b58ae143288a78488237115d5cfe9817b8373a5
LJS0000 <jisu2281l@naver.com>
2023-09-03 13:13:43 +0900

    ✨ FEATURE: 검색 페이지 라우팅

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <Link to='/'>
+          <Link to='/search'>
             <i className='mr-2 fas fa-search fa-2x'></i>
             <span className='hidden md:inline-block'>검색</span>
           </Link>
