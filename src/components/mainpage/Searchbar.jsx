ca65ec888a741971b925351e4c2a9bdfcf301a0b
LJS0000 <jisu2281l@naver.com>
2023-09-04 17:19:42 +0900

    💄 CSS: 검색창 CSS

--- src/components/mainpage/Searchbar.jsx
+++ src/components/mainpage/Searchbar.jsx
@@ -9,17 +9,15 @@ const Searchbar = ({ query, setQuery }) => {
   };
 
   return (
-    <div>
-      <form>
+    <div className='p-4'>
+      <form className='flex items-center p-2 bg-gray-200 rounded-lg'>
         <input
           type='text'
           placeholder='게시글 또는 @닉네임을 검색하세요'
           value={query.search}
           onChange={handleQuerySubmit}
+          className='flex-grow p-2 bg-transparent rounded-lg focus:outline-none'
         />
-        <button type='submit'>
-          <i className='fas fa-search'></i>
-        </button>
       </form>
     </div>
   );
