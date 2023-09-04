2585d0424903e1160f38ea861c1a0d2d68f2b3fd
LJS0000 <jisu2281l@naver.com>
2023-09-04 21:15:56 +0900

    💄 CSS: 게시글 목록 컴포넌트 CSS

--- src/components/mainpage/ListCard.jsx
+++ src/components/mainpage/ListCard.jsx
@@ -5,35 +5,41 @@ const ListCard = ({ posts }) => {
   const navigate = useNavigate();
 
   return (
-    <main>
-      <article className='flex flex-col gap-4'>
+    <main className='p-4 space-y-4'>
+      <article className='space-y-6'>
         {posts?.map((post, id) => {
           return (
             <div
               key={id}
-              id='postCard'
-              className='flex flex-col gap-2 p-4 rounded-lg'
+              className='flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md cursor-pointer hover:bg-gray-50'
               onClick={() => navigate(`/detail/${post.id}`)}
             >
-              <div className='flex items-center gap-2'>
+              <div className='flex items-center gap-4'>
                 <img
                   src={post.writer.image_url || profilePlaceholder}
                   alt='프로필사진'
-                  className='w-12 h-12 rounded-full'
+                  className='w-12 h-12 border border-gray-200 rounded-full'
                 />
-                <p className='text-lg'>{post.writer.username}</p>
+                <p className='text-lg font-medium'>@ {post.writer.username}</p>
               </div>
-              <div className='cursor-pointer'>
-                <div className='mb-4 text-lg'> {post.content}</div>
-                <img
-                  src={post.image_urls[0]?.image_url}
-                  alt='게시글 사진'
-                  className='max-w-[50vh] h-[40vh] object-cover rounded-lg shadow-md'
-                />
+              <div className='overflow-x-scroll hide-scrollbar'>
+                <div className='flex gap-2'>
+                  {post.image_urls.map((imageUrl, imgId) => {
+                    return (
+                      <div key={imgId} className='relative flex-none'>
+                        <img
+                          src={imageUrl.image_url}
+                          alt='게시글 사진'
+                          className='w-[50vh] h-[40vh] object-cover rounded-md shadow-lg'
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
-              <div className='flex items-center gap-2'>
+              <div className='flex items-center gap-4'>
                 <i className='text-red-500 far fa-heart'></i>
-                {post.like.length}
+                <span>{post.like.length}</span>
               </div>
             </div>
           );
