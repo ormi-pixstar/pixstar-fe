1cbf8275775ae3358b938f5b4645c0df6a9c25c1
LJS0000 <jisu2281l@naver.com>
2023-09-06 20:01:40 +0900

    ✏️ FIX: 로그인, 회원가입 navigate 추가

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { __signup, __login } from '../redux/modules/user.js';
 import AuthForm from '../components/authpage/AuthForm.jsx';
 
 const AuthPage = ({ formType }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const submitAuthData = async (authData) => {
     await dispatch(
       formType === 'signup' ? __signup(authData) : __login(authData)
     )
       .then((res) => {
-        console.log(res);
+        formType === 'signup' ? navigate('/login') : navigate('/');
       })
       .catch((err) => {
         alert('서버 연결에 문제가 발생했습니다.');
