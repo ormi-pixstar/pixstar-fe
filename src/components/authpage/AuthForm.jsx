33c565c136308c6397ac0ab17a2d3d9e736c79ec
LJS0000 <jisu2281l@naver.com>
2023-09-09 01:08:24 +0900

    ⚡️ IMPRV: 로그인 회원가입 코드 개선

--- src/components/authpage/AuthForm.jsx
+++ src/components/authpage/AuthForm.jsx
@@ -38,39 +38,48 @@ const AuthForm = ({ formType, onSubmit }) => {
   };
 
   return (
-    <form onSubmit={submitAuthForm} className='space-y-4'>
-      <input
-        name='email'
-        placeholder='이메일'
-        type='email'
-        value={authData.email}
-        onChange={handleInputChange}
-        className='w-full px-4 py-2 border rounded-md focus:border-blue-400'
-      />
-      <input
-        name='password'
-        placeholder='비밀번호'
-        type='password'
-        value={authData.password}
-        onChange={handleInputChange}
-        className='w-full px-4 py-2 border rounded-md focus:border-blue-400'
-      />
-      {formType === 'login' ? null : (
-        <>
+    <div className='flex items-center justify-center min-h-screen px-4 pb-12 bg-gray-50 sm:px-6 lg:px-8'>
+      <div className='w-full max-w-md space-y-8'>
+        <div>
+          <h2 className='text-3xl font-extrabold text-center text-gray-900 '>
+            {formType === 'signup' ? '회원가입' : '로그인'}
+          </h2>
+        </div>
+        <form onSubmit={submitAuthForm} className='space-y-4'>
           <input
-            name='confirm_password'
-            placeholder='비밀번호 재확인'
-            type='password'
-            value={authData.confirm_password}
+            name='email'
+            placeholder='이메일'
+            type='email'
+            value={authData.email}
             onChange={handleInputChange}
             className='w-full px-4 py-2 border rounded-md focus:border-blue-400'
           />
-        </>
-      )}
-      <AuthFormBtn
-        name={formType === 'login' ? '로그인' : '회원가입'}
-      />
-    </form>
+          <input
+            name='password'
+            placeholder='비밀번호'
+            type='password'
+            value={authData.password}
+            onChange={handleInputChange}
+            className='w-full px-4 py-2 border rounded-md focus:border-blue-400'
+          />
+          {formType === 'login' ? null : (
+            <>
+              <input
+                name='confirm_password'
+                placeholder='비밀번호 재확인'
+                type='password'
+                value={authData.confirm_password}
+                onChange={handleInputChange}
+                className='w-full px-4 py-2 border rounded-md focus:border-blue-400'
+              />
+            </>
+          )}
+          <AuthFormBtn
+            name={formType === 'login' ? '로그인' : '회원가입'}
+          />
+        </form>
+      </div>
+    </div>
   );
 };
 export default AuthForm;
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -12,7 +12,9 @@ const AuthPage = ({ formType }) => {
       formType === 'signup' ? __signup(authData) : __login(authData)
     )
       .then((res) => {
-        formType === 'signup' ? navigate('/login') : navigate('/');
+        if (res.type === 'signup/fulfilled' || res.type === 'login/fulfilled') {
+          formType === 'signup' ? navigate('/login') : navigate('/');
+        }
       })
       .catch((err) => {
         alert('서버 연결에 문제가 발생했습니다.');
@@ -20,18 +22,7 @@ const AuthPage = ({ formType }) => {
       });
   };
 
-  return (
-    <div className='flex items-center justify-center min-h-screen px-4 pb-12 bg-gray-50 sm:px-6 lg:px-8'>
-      <div className='w-full max-w-md space-y-8'>
-        <div>
-          <h2 className='text-3xl font-extrabold text-center text-gray-900 '>
-            {formType === 'signup' ? '회원가입' : '로그인'}
-          </h2>
-        </div>
-        <AuthForm formType={formType} onSubmit={submitAuthData} />
-      </div>
-    </div>
-  );
+  return <AuthForm formType={formType} onSubmit={submitAuthData} />;
 };
 
 export default AuthPage;
