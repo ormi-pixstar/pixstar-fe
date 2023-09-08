5661c96d14f69f0c796ceb30784298f3d3399440
LJS0000 <jisu2281l@naver.com>
2023-09-08 16:47:41 +0900

    💄 CSS: 버튼 컴포넌트 CSS

--- src/components/common/AuthFormBtn.jsx
+++ src/components/common/AuthFormBtn.jsx
@@ -1,4 +1,8 @@
 const AuthFormBtn = ({ name }) => {
-  return <button>{name}</button>;
+  return (
+    <button className='px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50'>
+      {name}
+    </button>
+  );
 };
 export default AuthFormBtn;
