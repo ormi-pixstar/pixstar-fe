e5bfd4db13773bbc676d07144eb475971a8ccff2
LJS0000 <jisu2281l@naver.com>
2023-09-17 19:35:26 +0900

    💄 CSS: 댓글 CSS 수정

--- src/components/detailpage/CommentCard.jsx
+++ src/components/detailpage/CommentCard.jsx
@@ -3,23 +3,22 @@ import convertToKoreanDate from '../../utils/convertToKoreanDate.js';
 const CommentCard = ({ comment }) => {
   const formattedDate = convertToKoreanDate(comment.created_at);
 
-  console.log(comment);
-
   return (
-    <div>
-      <div className='flex items-center gap-4'>
-        <p>@{comment.writer.username}</p>
-        <p className='text-xl'>{comment.comment}</p>
-        <p>{formattedDate}</p>
+    <div className='mb-4'>
+      <div className='flex items-center gap-4 p-3 bg-white rounded-lg shadow-md'>
+        <p className='text-blue-600'>@{comment.writer.username}</p>
+        <p className='flex-grow text-xl'>{comment.comment}</p>
+        <p className='text-sm text-gray-500'>{formattedDate}</p>
       </div>
-      <div className='p-4 ml-4 border-l-2'>
-        {comment.reply
-          ? comment.reply?.map((comment, i) => {
-              return <CommentCard key={i} comment={comment} />;
-            })
-          : null}
-      </div>
+      {comment.reply && (
+        <div className='mt-2 ml-6'>
+          {comment.reply.map((replyComment, i) => (
+            <CommentCard key={i} comment={replyComment} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
+
 export default CommentCard;
